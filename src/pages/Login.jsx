a9f007e10c4f6905667b2db1b64b74017a035f7b
Zinaida Rudak <rudakzinaida@gmail.com>
2025-12-22 18:07:58 +0300

    feat: Add auth state with Redux Toolkit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../features/auth/authSlice';
+import React from 'react'
+
 function Login() {
-  return <h2>Login Page</h2>;
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(
+    (state) => state.auth.isAuthenticated
+  );
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+
+      {isAuthenticated
+        ? (<p>You are logged in</p>)
+        : (<button aria-label="Authentification button" onClick={() => dispatch(login())}> Login </button> )
+      }
+    </div>
+  );
 }
 
 export default Login;
